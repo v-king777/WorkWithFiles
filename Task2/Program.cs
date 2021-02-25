07af46917f2cfdeb3c9247bf5d6275d0a6998227
v-king777 <v.kolpashchikov@gmail.com>
2021-02-25 21:41:08 +0300

    modified:   Task2/Program.cs

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Task2
 {
@@ -6,7 +7,40 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string folderPath = Path.Combine("E:", "WorkWithFiles", "Task2", "TestFolder");
+            double folderSize = 0;
+
+            FolderSizeCalculation(folderPath, ref folderSize);
+
+            Console.WriteLine("Размер папки: {0} байт", folderSize);
+        }
+
+        static void FolderSizeCalculation(string path, ref double size)
+        {
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("Указанного пути не существует");
+                return;
+            }
+
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+                foreach (var file in dirInfo.GetFiles())
+                {
+                    size += file.Length;
+                }
+
+                foreach (var dir in dirInfo.GetDirectories())
+                {
+                    FolderSizeCalculation(dir.FullName, ref size);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Произошла ошибка: " + e.Message);
+            }
         }
     }
 }
